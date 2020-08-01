65e2c970e7c4a9525b5f6624b7e2bf0745fb7d94
Jhonattas Henrique <jhonattas@soucriador.com>
2020-08-01 19:29:21 -0300

    update the main screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://blog.soucriador.com/images/criador_colorido.png" className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://soucriador.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sou Criador
         </a>
       </header>
     </div>
